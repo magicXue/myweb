5a02d8008f98523624c28f3e34e158cfc99dfaa0
Xch\david <davidchenxue@gmail.com>
2022-03-30 21:55:15 +1100

    my website

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const projects = [
+    {
+      title: "PC-Gaming",
+      subtitle: "Vue and Python",
+      description:
+        "Pcgaming designed for e-commerce website developing by vue, vuex in the front end, django at the back end. Building the homepage, categories and checkout page. It can create new category or even list the products at Backend system",
+      image: "../img/pcgaming.png",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "../img/calculator.png",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "CourseMapping",
+      subtitle: "Vue and Cakephp",
+      description:
+        "CourseMapping designed for mapping course between two universities developing by vue, vuex and vuetify in the front end, cakephp at the back end. Skilled in displaying vuetify's datatable",
+      image: "../img/courseMapping.png",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Self-application and Admin Management",
+      subtitle: "Vue and cakephp",
+      description:
+        "This system aim to help applicant to apply offer including signin and signup, uploading  documents and also admin manage applications including approve application ,auto generate offer and sending notification email",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+export const skills = [
+    "Vue/vuex","PHP","JavaScript","JQuery","Nodejs","React"
+];
+export const testimonials = [
+    {
+        quote: "React Reserve",
+        name: "MERN Stack",
+        company:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+    },
+];
